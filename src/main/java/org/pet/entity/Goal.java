952cde1364a5b5903853da948d46536a8a075859
Oleksandr Rodin <rodiamix@gmail.com>
2020-09-24 17:02:34 +0300

    adding userDetails

--- src/main/java/org/pet/entity/Goal.java
+++ src/main/java/org/pet/entity/Goal.java
@@ -1,15 +1,17 @@
 package org.pet.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Goal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
     private String description;
 
+    @OneToOne
+    private UserDetails userDetails;
 }
