ee28f2a8b9d0aa2520b96b551983dd8f9dce920e
Oleksandr Rodin <rodiamix@gmail.com>
2020-09-24 17:15:04 +0300

    first security commit: adding validator package

---
+++ src/main/java/org/pet/validator/UserValidator.java
@@ -0,0 +1,18 @@
+package org.pet.validator;
+
+import org.pet.entity.User;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class UserValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+    }
+}
