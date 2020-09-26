e01802eabbc4763db3e9bfcbbb16ed7fcb1f2094
Oleksandr Rodin <rodiamix@gmail.com>
2020-09-26 11:31:16 +0300

    edit validator

--- src/main/java/org/pet/validator/UserValidator.java
+++ src/main/java/org/pet/validator/UserValidator.java
@@ -15,6 +15,14 @@ public class UserValidator implements Validator {
     public void validate(Object o, Errors errors) {
         User user = (User) o;
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "Required");
+        if (user.getUsername().length() < 8 || user.getUsername().length() > 32) {
+            errors.rejectValue("username", "Size.userForm.username");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "Required");
+        if (user.getPassword().length() <= 8) {
+            errors.rejectValue("password", "Size.userForm.password");
+        }
 
     }
 }
