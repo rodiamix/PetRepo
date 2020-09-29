118b649edef6e7ea36fbf8abb4ab1ad64983b72e
Oleksandr Rodin <rodiamix@gmail.com>
2020-09-29 10:22:56 +0300

    validator commit

--- src/main/java/org/pet/validator/UserValidator.java
+++ src/main/java/org/pet/validator/UserValidator.java
@@ -24,5 +24,8 @@ public class UserValidator implements Validator {
             errors.rejectValue("password", "Size.userForm.password");
         }
 
+        if (!user.getConfirmPassword().equals(user.getPassword())) {
+            errors.rejectValue("confirmPassword", "Different.userForm.password");
+        }
     }
 }
